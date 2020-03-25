71e68bc138cbef3436a2116109c923ae10ad8b81
SSophien <samsophien@gmail.com>
2020-03-25 15:51:55 +0100

    Added GridLayout RecyclerView

--- app/src/main/java/mobile/dev/catwiki/MainActivity.kt
+++ app/src/main/java/mobile/dev/catwiki/MainActivity.kt
@@ -3,12 +3,14 @@ package mobile.dev.catwiki
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity(), OnItemClickListener {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var gridLayoutManager: GridLayoutManager
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var listCatBreeds: ArrayList<CatBreed>
@@ -17,29 +19,31 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var breed1 = CatBreed("Abyssinian cat","Small to medium, with males weighing 7 to 10 pounds and females weighing 6 to 8 pounds","Medium","Ruddy, red, blue, fawn","9 to 15 years")
-        var breed2 = CatBreed("American Bobtail","Medium to large, with males weighing 12 to 16 pounds and females weighing 7 to 11 pounds ","Medium (shorthair) and long (longhair) ","White, black, blue, red, cream, chocolate, lavender, cinnamon, fawn, plus various patterns and shadings","")
-        var breed3 = CatBreed("Bengal","","","","")
-        var listCatBreeds = ArrayList<CatBreed>()
-        listCatBreeds.add(breed1)
-        listCatBreeds.add(breed2)
-        listCatBreeds.add(breed3)
-        listCatBreeds.add(breed3)
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = BreedAdapter(listCatBreeds, this)
+        listCatBreeds = ArrayList()
+        listCatBreeds = setDataInList()
 
-        recyclerView = findViewById<RecyclerView>(R.id.cats_recycler_view).apply {
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = BreedAdapter(listCatBreeds!!, this)
+
+        recyclerView = findViewById<RecyclerView>(R.id.cats_recycler_view)
+        gridLayoutManager = GridLayoutManager(applicationContext, 2, LinearLayoutManager.VERTICAL, false)
+
+
+        recyclerView.apply {
             // use this setting to improve performance if you know that changes
             // in content do not change the layout size of the RecyclerView
             setHasFixedSize(true)
 
             // use a linear layout manager
-            layoutManager = viewManager
+            //layoutManager = viewManager
+            layoutManager = gridLayoutManager
 
             // specify an viewAdapter (see also next example)
             adapter = viewAdapter
 
         }
+
+
     }
 
     override fun onItemClick(item: CatBreed, position: Int) {
@@ -51,8 +55,49 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         intent.putExtra("BREEDLIFESPAN", item.lifespan)
         //intent.putExtra("BREEDIMAGE", item.image.toString())
         startActivity(intent)
+    }
+
+    private fun setDataInList(): ArrayList<CatBreed>{
+        var breed1 = CatBreed("Abyssinian cat","Small to medium, with males weighing 7 to 10 pounds and females weighing 6 to 8 pounds","Medium","Ruddy, red, blue, fawn","9 to 15 years")
+        var breed2 = CatBreed("American Bobtail","Medium to large, with males weighing 12 to 16 pounds and females weighing 7 to 11 pounds ","Medium (shorthair) and long (longhair) ","White, black, blue, red, cream, chocolate, lavender, cinnamon, fawn, plus various patterns and shadings","")
+        var breed3 = CatBreed("Bengal","","","","")
+        var listCatBreeds = ArrayList<CatBreed>()
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
+        listCatBreeds.add(breed1)
+        listCatBreeds.add(breed2)
+        listCatBreeds.add(breed3)
 
 
+        return listCatBreeds
     }
 
 }
